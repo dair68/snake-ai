d11dc2c13cc6582791b336aa2bac58cbad4216fd
dair68 <granthuang68@yahoo.com>
2022-06-06 22:38:58 -0700

    basic movement

--- snake.py
+++ snake.py
@@ -32,28 +32,84 @@ class SnakeGame:
         canvasHeight = self.squareLength*self.rows
         canvasWidth = self.squareLength*self.cols
         self.canvas = Canvas(gameFrame, height=canvasHeight, width=canvasWidth)
-        self.grid[10][10] = 1
+        self.canvas.configure(bg="black")
         self.canvas.bind("<Up>", self.up)
         self.canvas.focus_set()
-        self.redrawGame() 
         self.canvas.pack()
         
-        self.gameStarted = False
+        self.snakeMoving = False
         
-        self.headRow = 0
         self.headCol = 0
+        self.headRow = 0
         self.headXVelocity = 0
         self.headYVelocity = 0
+        self.tailCol = 0
+        self.tailRow = 0
+        self.prevTailCol = 0
+        self.prevTailRow = 0
+        self.pellet = None
+        self.pelletCol = 0
+        self.pelletRow = 0
+        self.snake = []
         
-    #draws a white snake unit square within game area
+        self.start(10, 10)
+        
+    #begins a game of snake with the start snake segment at a certain position
+    #@param col - column number of start snake segment. number from 0-19.
+    #@param row - row number of start snake segment. number from 0-19
+    def start(self, col=0, row=0):
+        self.grid[col][row] = 1
+        self.headCol = col
+        self.headRow = row
+        self.tailCol = self.headCol
+        self.tailRow = self.headRow
+        self.prevTailCol = self.tailCol
+        self.prevTailRow = self.tailRow
+        
+        startSquare = self.drawUnitSquare(col, row)
+        self.snake.append(startSquare)
+        
+    #draw white unit square in game area
     #@param col - column number from 0 to 19
     #@param row - row number from 0 to 19
-    def drawSnakeSquare(self, col, row):
+    #returns reference to square drawn
+    def drawUnitSquare(self, col, row):
         k = self.squareLength
         x = col*k
         y = row*k
-        self.canvas.create_rectangle(x, y, x + k, y + k, fill="white")
+        square = self.canvas.create_rectangle(x, y, x + k, y + k)
+        self.canvas.itemconfigure(square, fill="white", outline="white")
         self.canvas.pack()
+        
+        return square
+        
+    #draws a white unit square that will be treated as pellet for snake to eat
+    #@param col - column number from 0 to 19
+    #@param row - row number from 0 to 19
+    def drawPellet(self, col, row):
+        self.pelletCol = col
+        self.pelletRow = row
+        self.grid[col][row] = 1
+        self.pellet = self.drawUnitSquare(col, row)
+        self.canvas.pack()
+        
+    #has the snake eat the pellet currently on screen to elongate it
+    def eatPellet(self):
+        self.grid[self.pelletCol][self.pelletRow] = 0
+        self.pelletCol = -1
+        self.pelletRow = -1
+        self.pellet = None
+        
+        self.snake.append(self.pellet)
+        self.grid[self.prevTailCol][self.prevTailRow] = 1
+        self.tailCol = self.prevTailCol
+        self.tailRow = self.prevTailRow
+        
+    #removes white unit square from game area
+    #@param col - column number from 0 to 19
+    #@param row - row number from 0 to 19
+    def eraseSnakeSquare(self, col, row):
+        pass
         
     #redraws game area to match current progress
     def redrawGame(self):
@@ -64,7 +120,7 @@ class SnakeGame:
            for j in range(self.rows):
                #drawing white square where there's a 1 in grid
                if self.grid[i][j] == 1:
-                   self.drawSnakeSquare(i, j)
+                   self.drawPellet(i, j)
                    
        self.canvas.pack()
        
@@ -76,11 +132,23 @@ class SnakeGame:
         self.headXVelocity = 0
         
         #starting game if hasn't started yet
-       # if not self.gameStarted:
-       #     self.gameStarted = True
-       #     self.moveSnake()
+        if not self.snakeMoving:
+            self.snakeMoving = True
+            self.moveSnake()
             
     #moves the snake until game ends
-    def moveSnake():
+    def moveSnake(self):
+        self.headCol += self.headXVelocity
+        self.headRow += self.headYVelocity
+        tail = self.snake[-1]
+        self.grid[self.headCol][self.headRow] = 1
+        
+        k = self.squareLength
+        x = self.headCol*k
+        y = self.headRow*k
+ 
+        self.canvas.coords(tail, x, y, x + k, y + k)
+        self.grid[self.tailCol][self.tailRow] = 0
+            
         milliseconds = 1000
-        self.canvas.after(milliseconds, )
+        #self.canvas.after(milliseconds, self.moveSnake())
