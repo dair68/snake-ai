acf2120aef52e296a0e652b52c1d4b458247f7f8
dair68 <granthuang68@yahoo.com>
2023-05-18 17:14:56 -0700

    added another move map to advanced ai

--- ai/advancedAI.py
+++ ai/advancedAI.py
@@ -1,7 +1,7 @@
 #module hosting ai that will allow snake to win in reasonable timeframe
 from ai.snakeAI import SnakeAI
 from collections import deque
-from ai.surviveAI import SurviveAI
+import random
 
 class AdvancedAI(SnakeAI):
     #constructor
@@ -11,6 +11,23 @@ class AdvancedAI(SnakeAI):
         assert game.rows % 2 == 0
         assert game.cols % 2 == 0
         
+        moveMatrix = []
+        randNum = random.randrange(2)
+        
+        #checking which random number chosen
+        if randNum == 1:
+            moveMatrix = self.__clockwiseMoveMatrix(game)
+        else:
+            moveMatrix = self.__counterclockwiseMoveMatrix(game)
+        
+        #print(moveMatrix)
+        super().__init__(game, moveMatrix)
+        self.pelletPath = deque()
+        
+    #creates move matrix built from 2x2 clockwise squares
+    #@param game - snake game
+    #returns matrix mapping space coords to set of space coords to travel to
+    def __clockwiseMoveMatrix(self, game):
         r = game.rows + 2
         c = game.cols + 2
         moveMatrix = [[set() for j in range(r)] for i in range(c)]
@@ -34,9 +51,36 @@ class AdvancedAI(SnakeAI):
                     moveSet.add((i-1, j))
                     moveSet.add((i, j-1))
                     
-        #print(moveMatrix)
-        super().__init__(game, moveMatrix)
-        self.pelletPath = deque()
+        return moveMatrix
+    
+    #creates move matrix built from 2x2 counter-clockwise squares
+    #@param game - snake game
+    #returns matrix mapping space coords to set of space coords to travel to
+    def __counterclockwiseMoveMatrix(self, game):
+        r = game.rows + 2
+        c = game.cols + 2
+        moveMatrix = [[set() for j in range(r)] for i in range(c)]
+        
+        #filling moveMatrix
+        for i in range(1, game.cols + 1):
+            for j in range(1, game.rows + 1):
+                moveSet = moveMatrix[i][j]
+                
+                #assigning move based on col row pair
+                if i % 2 == 1 and j % 2 == 1:
+                    moveSet.add((i-1, j))
+                    moveSet.add((i, j+1))
+                elif i % 2 == 0 and j % 2 == 1:
+                    moveSet.add((i, j-1))
+                    moveSet.add((i-1, j))
+                elif i % 2 == 0 and j % 2 == 0:
+                    moveSet.add((i+1, j))
+                    moveSet.add((i, j-1))
+                else:
+                    moveSet.add((i, j+1))
+                    moveSet.add((i+1, j))
+                    
+        return moveMatrix
         
     #computes a path to the pellet from snake's current state
     #returns deque of space coords for path from snake head to pellet
@@ -63,10 +107,10 @@ class AdvancedAI(SnakeAI):
     #   run self.update() after following move returned by function
     def nextMove(self):
         print("advanced ai move")
+         
+        #checking if pellet path exists
         if not self.pelletPath:
              self.pelletPath = self.findPelletPath()
-             print("pellet path: ")
-             print(self.pelletPath)
              self.pelletPath.popleft()
-             
+        
         return self.pelletPath.popleft()
