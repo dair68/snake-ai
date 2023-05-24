f620e1d0008a3e34ae193e9e1707c311f710d781
dair68 <granthuang68@yahoo.com>
2023-05-23 19:09:00 -0700

    added app icon

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ Created on Mon May 23 22:29:28 2022
 @author: grant
 """
 
+import tkinter as tk
 from snake import SnakeGame
-from tkinter import Tk
 
-root = Tk()
+root = tk.Tk()
 root.title("Snake")
 root.resizable(False, False)
+root.iconbitmap("snakeicon.ico")
 game = SnakeGame(root)
 root.mainloop()
