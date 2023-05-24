ed71b5fe8bb1b5deaa62f7be948e894f0fdf2a57
dair68 <granthuang68@yahoo.com>
2023-05-23 20:13:18 -0700

    created executable

--- main.py
+++ main.py
@@ -5,12 +5,24 @@ Created on Mon May 23 22:29:28 2022
 @author: grant
 """
 
+import sys
+import os
 import tkinter as tk
 from snake import SnakeGame
 
 root = tk.Tk()
 root.title("Snake")
 root.resizable(False, False)
-root.iconbitmap("snakeicon.ico")
+icon = "snakeicon.ico"
+iconPath = ""
+
+try:
+    basePath = sys._MEIPASS
+except Exception:
+    basePath = os.path.abspath(".")
+finally:
+    iconPath = os.path.join(basePath, icon)
+    
+root.iconbitmap(iconPath)
 game = SnakeGame(root)
 root.mainloop()
